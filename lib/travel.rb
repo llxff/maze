24697b34922624b60d603e266b63f8b299eed379
llxff <ll.wg.bin@gmail.com>
2014-03-19 00:37:29 +0400

    target found

--- lib/travel.rb
+++ lib/travel.rb
@@ -12,18 +12,26 @@ class Travel
     until road.empty?
       current_point = road.shift
 
-      if current_point.x == target_point.x and current_point.y == target_point.y
+      if target_found?(current_point, target_point)
         return current_point
       else
         x = current_point.x
         y = current_point.y
+
         map.visited_point(x, y)
+
         road << Point.new(x + 1, y, current_point) if map.have_point_at?(x + 1, y)
         road << Point.new(x - 1, y, current_point) if map.have_point_at?(x - 1, y)
         road << Point.new(x, y + 1, current_point) if map.have_point_at?(x, y + 1)
         road << Point.new(x, y - 1, current_point) if map.have_point_at?(x, y - 1)
       end
     end
+  end
+
+private
+
+  def target_found?(current_point, target_point)
+    current_point.x == target_point.x and current_point.y == target_point.y
   end
 
 end
